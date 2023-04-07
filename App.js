1f6f51a4ffd78b3f3ece3627e09ca506918224a2
Brenda <brendaferreirasantos26@gmail.com>
2023-04-07 17:25:44 -0300

    adicionar e remover

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View,TextInput,SafeAreaView, FlatList, TouchableOpacity,Text } from 'react-native';
+
+import Pessoa from './listas';
 
 export default function App() {
+
+  const [nome, setNome] = useState('');
+  const [lista, setLista] = useState([]);
+
+  function adicionar() {
+    const novoItem = { nome };
+    setLista([ ...lista, novoItem]);
+  }
+
+  function removerItem(index) {
+    const novaLista = [...lista];
+    novaLista.splice(index,1);
+    setLista(novaLista);
+  }
+  
+ 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+      <TextInput
+      style={styles.input}
+      placeholder="Digite uma tarefa"
+      onChangeText={(text) => setNome(text)}
+      value={nome}
+      
+    
+      />
+      <TouchableOpacity onPress={adicionar}><Text style={styles.button}>+</Text></TouchableOpacity>
+      <TouchableOpacity onPress={removerItem}><Text style={styles.button}>-</Text></TouchableOpacity>
+      
+      </View>
+      
+      <FlatList
+      data={lista}
+      renderItem={({ item }) => <Pessoa data={item} />}
+     
+      
+    />
+
+      
+   
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection:'row',
+   
+  },
+  input:{
+    height:40,
+   
+    borderWidth:3,
+    margin:10,
+    padding:10,
+    fontSize:20
+  },
+  texto:{
+    fontSize:10,
+    justifyContent:'center'
+  },
+  button: {
+    fontSize: 20,
+    color: 'blue',
+    margin: 10,
   },
 });
+
+
+
+
+
---
+++ listas.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import{Text,StyleSheet,View} from 'react-native';
+
+
+
+
+function Pessoa({data}){
+    return(
+        <View style={styles.areaPessoa}>
+        <Text style={styles.textoPessoa}>{data.nome}</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    areaPessoa:{
+        flex:1,
+    },
+    textoPessoa:{
+        fontSize:15,
+        color:'black'
+
+    }
+})
+
+export default Pessoa;
